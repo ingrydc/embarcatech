bf8542fd7a4e57ca3db66ed7c0507b8436692761
Ingryd <116104947+ingrydc@users.noreply.github.com>
2024-12-12 16:23:33 +0000

    coments

--- notes/exStruct.c
+++ notes/exStruct.c
@@ -1,15 +1,15 @@
 /* exemplo básico */
 
-#include <stdio.h>
+#include <stdio.h>  // inclui biblioteca padrao, necessaria para a funcao printf
 
-struct Aluno 
+struct Aluno // define a estrutura, q eh usada para agrupar diferentes tipos de dados
 {
     char nome[50];
     int matricula;
     float nota;
 };
 
-int main() {
+int main() { // aqui o codio cria a variavel aluno1 do tipo struct Aluno e inicializa os 3 campos
     struct Aluno aluno1 = {"João", 12345, 9.5};
 
     printf("Nome: %s\n", aluno1.nome);
